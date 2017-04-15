48d83ed7778652dddc1b4625f34284f0d91f1c80
rocampo3 <rocampo3@student.gsu.edu>
2017-04-15 13:49:26 -0400

    extracted HRMS.zip contents into Front-endHRMS folder

---
+++ Front-endHRMS/HRMS/HRMS/index.php
@@ -0,0 +1,49 @@
+<!-- this is the login page which will be the first page(index page) for our Project -->
+
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+?>
+
+<link rel="stylesheet" href="register.css" type="text/css">
+<br> 
+<img id= "logo" src="logo.png" width="240" height="100" title="Logo of a company" alt="Logo of a company" />
+</br> 
+<HR WIDTH="100%" COLOR="#6699FF" SIZE="30">
+<HR WIDTH="100%" SIZE="3"> 
+
+<!-- body for registration  -->
+<link rel="stylesheet" href="style.css" type="text/css">
+<div class="register-form">
+  <div class="module">
+    <h1 id="adL">ADMIN LOGON</h1>
+    <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off"> 
+      User Name:
+      <input type="text" name="username" placeholder="Username"> 
+      <br><br>
+      Password: &emsp;  
+      <input type="text" name="password" placeholder="Password">
+      <br> <br> <br>
+    <input type="submit" name="login" value="Login">
+    <h5><a href="register.php">New User Registration</a>
+</h5>
+     
+</a>
+
+  <br>
+  <br>
+
+    </form>
+  </div>
+
+<!-- end body  -->
+
+</div>
