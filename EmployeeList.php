e66b382d699a2e6737160629bdc2721ea9c016a8
mterfie1 <mechalalemuterfie@yahoo.com>
2017-04-17 04:01:27 -0400

    Add files via upload

--- EmployeeList.php
+++ EmployeeList.php
@@ -1,5 +1,16 @@
 <?php
 
+	//making connection
+	mysql_connect('localhost', 'root', '');
+
+	//selecting database: enter database name
+	mysql_select_db('dbms');
+	
+	//table
+	$sql="SELECT * FROM employee"
+
+	$records=mysql_query($sql);
+
 
 ?>
 
@@ -24,10 +35,82 @@
 <link rel="stylesheet" href="style.css" type="text/css">
 <div class="register-form">
   <div class="module">
-    <h1>employee list... add your code :) </h1>
-    <h1>Mike just a little note for you, I used one css file for all my pages. I would recommend to create a new css file for your pages . copy everything from my style.css to your css file (I know u wont need evrything from my css file but leave it there for the default page layout). that way we wont override each other styles for the pages  :) </h1>
+  
+  <head>
+  <title>Employee List</title>
+  </head>
+
+<BODY bgcolor ="pink">
+	<table width="600" border="1" cellpadding="1" cellspacing="1">
+	<tr>
+	<th>Fname</th>
+    <th>MI:</th>
+	<th>Lname</th>
+	<th>Gender</th>
+	<th>SSN</th>
+	<th>DOB</th>
+    <th>Address</th>
+	<th>Ethnicity</th>
+	<th>Phone</th>
+	<th>EmergCont</th>
+	<th>Email</th>
+    <th>HiredDate</th>
+	<th>Position</th>
+	<th>SupervName</th>
+	<th>JobType</th>
+
+	</table>
+	<br> 
+
+	<?php
+	while($employee=mysql_fetch_assoc($records)){
+		
+		echo "<tr>";
+		
+		echo "<td>".$employee['Fname']."</td>";
+		
+		echo "<td>".$employee['MI']."</td>";
+		
+		echo "<td>".$employee['Lname']."</td>";
+
+		echo "<td>".$employee['Gender']."</td>";
+		
+		echo "<td>".$employee['SSN']."</td>";
+		
+		echo "<td>".$employee['DOB']."</td>";
+		
+		echo "<td>".$employee['Address']."</td>";
+		
+		echo "<td>".$employee['Ethnicity']."</td>";
+		
+		echo "<td>".$employee['Phone']."</td>";
+		
+		echo "<td>".$employee['EmergCont']."</td>";
+		
+		echo "<td>".$employee['Email']."</td>";
+		
+		echo "<td>".$employee['HiredDate']."</td>";
+		
+		echo "<td>".$employee['Position']."</td>";
+		
+		echo "<td>".$employee['SupervName']."</td>";
+		
+		echo "<td>".$employee['JobType']."</td>";
+
+		
+		
+		echo "</tr>";
+		
+	   	
+	}
+	?>
+	
+	</body>
+	
   </div>
 
 <!-- end body  -->
 
 </div>
+</body>
+</html>
