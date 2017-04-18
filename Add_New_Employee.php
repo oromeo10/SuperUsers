e109eb746cced819da5c3b1be6a380276b1ce924
dk727 <dalya727@yahoo.com>
2017-04-18 11:24:59 -0400

    added departments, positions, and disability

--- Add_New_Employee.php
+++ Add_New_Employee.php
@@ -31,16 +31,19 @@
   <br>
 
   Gender:
-  <input type="radio" name="gender" value="male" > Male
-  <input type="radio" name="gender" value="female"> Female
-  <input type="radio" name="gender" value="other"> Other
+  <input type="radio" name="gender" value="M" > MALE
+  <input type="radio" name="gender" value="F"> FEMALE
 
-  
+  <br><br>
+  Disability:
+  <input type="radio" name="YesNo" value="Y" > YES
+  <input type="radio" name="YesNo" value="N"> NO
+
   <br><br>
   SSN:
   <input type="text" name="ssn" placeholder="000000000">
   Date of Birth:
-  <input type="text" name="DateOfBirth" placeholder="MM/DD/YYYY">
+  <input type="text" name="DateOfBirth" placeholder="YYYY/MM/DD">
   <br> <br>
   Address:
   <input type="text" name="Street" placeholder="Street">
@@ -62,25 +65,39 @@
   </select>
 
   Phone Number:
-  <input type="text" name="PhoneNumber" placeholder="000-000-0000">
+  <input type="text" name="PhoneNumber" placeholder="0000000000">
   Emergency Contact:
-  <input type="text" name="EmergencyContact" placeholder="000-000-0000">
+  <input type="text" name="EmergencyContact" placeholder="0000000000">
   <br><br>
   Email:
   <input type="text" name="email" placeholder="example@something.com">
   Hired date:
-  <input type="text" name="HiredDate" placeholder="MM/DD/YYYY">
+  <input type="text" name="HiredDate" placeholder="YYYY-MM-DD">
   Position:
-  <input type="text" name="HiredDate" placeholder="Position Title">
+  <select>
+    <option>Select</option>
+    <option>Cashier</option>
+    <option>Manager</option>
+    <option>Security Oficer</option>
+    <option>Greeter</option>
+    <option>Customer Service Representative</option>
+    <option>Cashier Assistant</option>
+    <option>Intern</option>
+    <option>Assistant Manager</option>
+  
+  </select>
   <br><br>
   Supervisor Name:
   <input type="text" name="SupevisorName" placeholder="First and last">
   Department Name:
   <select>
     <option>Select</option>
-    <option>Cashier</option>
-    <option>Manager</option>
+    <option>Customer Service</option>
     <option>Finance</option>
+    <option>Pharmacy</option>
+    <option>Bakery</option>
+    <option>Meat</option>
+    <option>Seafood</option>
     <option>IT</option>
     <option>HR</option>
     <option>Accounting</option>
@@ -93,14 +110,11 @@ Job Type:
     <option>Select</option>
     <option>Part Time</option>
     <option>Full Time</option>
-
     <option>Allowed Overtime</option>
   </select>
 <!-- submit emplyee information -->
 <br><br>
-
     <input type="submit" name="AddEmployee" value="Add Employee">
-
 
 <br><br>
 </form>
